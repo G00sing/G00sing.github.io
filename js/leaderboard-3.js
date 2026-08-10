a76be1ce97a5d9cc7ab47d1e359f5ea2d0a14e52
G00sing <justinkuelling@gmail.com>
2026-08-10 17:05:01 -0400

    update for 2026 info

---
+++ js/leaderboard-3.js
@@ -0,0 +1,165 @@
+function getTabOneRecords() {
+    const tabOneRecords = [
+        {
+            image: "./images/specific/jobu.jpg",
+            name: "JoBu",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/top-spin.jpg",
+            name: "Top Spin",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/soco.jpg",
+            name: "SoCo",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/miller-time.jpg",
+            name: "Miller Time",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/grizz.jpg",
+            name: "Silent Assassin",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/short-grass.jpg",
+            name: "Short Grass",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/burnsie.jpg",
+            name: "Burnsie",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+        {
+            image: "./images/specific/brick-house.jpg",
+            name: "Brick House",
+            par: 0,
+            rd1: "-<br />-<br />-",
+            rd2: "-<br />-<br />-",
+            rd3: "-<br />-<br />-",
+            rd4: "-<br />-<br />-",
+            rd5: "-<br />-<br />-",
+            net: 0,
+            gross: 0,
+        },
+    ];
+
+    // Sort the rows by par (lowest par is position 1)
+    tabOneRecords.sort((a, b) => a.par - b.par);
+
+    const $container = $("#pills-tab1 .generated-records-2");
+    $container.empty();
+
+    tabOneRecords.forEach((item, idx) => {
+        const $row = $('<div class="row mb-3 border-bottom border-charcoal"></div>');
+
+        const parDisplay = item.par === 0 ? "E" : item.par > 0 ? `+${item.par}` : item.par;
+
+        const columns = [
+            // Position
+            `<div class="col-1 col-md-1 my-auto text-center txt-eggshell">${idx + 1}</div>`,
+            // Image
+            `<div class="col-2 col-md-3 mx-auto my-auto">
+                <p align="center">
+                    <img src="${item.image}" class="max-w-3em rounded-circle img-fluid w-100 border border-myrtle" alt="${item.name}">
+                    <span class="txt-eggshell"><strong>${item.name}</strong></span>
+                </p>
+            </div>`,
+            // Player Name
+            // `<div class="col-0 col-md-2 my-auto d-none d-md-block ">
+            //     <p>
+            //         <img src="${item.image}" class="max-w-2em rounded-circle img-fluid w-100 border border-myrtle" alt="${item.name}">
+            //         <span class="txt-eggshell"><strong>${item.name}</strong></span>
+            //     </p>
+            // </div>`,
+            // Par
+            `<div class="col-2 col-md-1 my-auto text-center"><p class="txt-blueberry"><strong>${parDisplay}</strong></p></div>`,
+            // RD's
+            `<div class="col-1 col-md-1 my-auto text-center"><p class="txt-eggshell">${item.rd1}</p></div>`,
+            `<div class="col-1 col-md-1 my-auto text-center"><p class="txt-eggshell">${item.rd2}</p></div>`,
+            `<div class="col-1 col-md-1 my-auto text-center"><p class="txt-eggshell">${item.rd3}</p></div>`,
+            `<div class="col-1 col-md-1 my-auto text-center"><p class="txt-eggshell">${item.rd4}</p></div>`,
+            `<div class="col-1 col-md-1 my-auto text-center"><p class="txt-eggshell">${item.rd5}</p></div>`,
+            // Net
+            `<div class="col-0 col-md-1 my-auto text-center d-none d-md-block"><p class="txt-neon-green"><strong>${item.net}</strong></p></div>`,
+            // Gross
+            `<div class="col-0 col-md-1 my-auto text-center d-none d-md-block"><p class="txt-apple"><strong>${item.gross}</strong></p></div>`,
+            // Net & Gross on Mobile
+            `<div class="col-2 col-md-0 my-auto text-center d-block d-md-none"><p class="txt-neon-green"><strong>${item.net}</strong></p><p class="txt-apple"><strong>${item.gross}</strong></p></div>`,
+        ];
+
+        // Add each column to the row
+        columns.forEach((column) => {
+            $row.append($(column));
+        });
+
+        // Add the row to the table (container)
+        $container.append($row);
+    });
+}
+
+$(document).ready(function () {
+    // Make the tab buttons work when clicked. When clicked they will
+    // show and hide some content.
+    $("#pills-tab a").on("click", function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+
+    // Load the records for tab 1
+    getTabOneRecords();
+});
